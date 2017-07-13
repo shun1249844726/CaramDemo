5c8db0132e456e1bbcb5547365cdc945fcdf6f33
shun1249844726 <1249844726@qq.com>
2017-07-13 13:55:00 +0800

    完成摄像头初始显示功能

---
+++ app/src/main/java/com/lexinsmart/cms/caramdemo/activity/LoginActivity.java
@@ -0,0 +1,16 @@
+package com.lexinsmart.cms.caramdemo.activity;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by xushun on 2017/7/13.
+ */
+
+public class LoginActivity extends AppCompatActivity{
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
